bd00b145355fff0c390f1802b07e2b3db08c3c95
bunrieucuaa <ann71576@gmail.com>
2024-04-25 00:08:05 +0700

    feat: init game

--- main.py
+++ main.py
@@ -9,10 +9,29 @@ class Game:
         self.screen = pg.display.set_mode(RES) #Tạo một cửa sổ trò chơi với kích thước được định nghĩa ở setting
         self.clock = pg.time.Clock() #Khởi tạo một đối tượng clock để theo dõi thời gian trong trò chơi
 
-
     def new_game(self):
         pass
 
     def update(self):
-        pg.display.flip()
-        self.clock.tick(FPS)
+        pg.display.flip() #Cập nhật màn hình với những thay đổi đã được vẽ.
+        self.clock.tick(FPS) # Điều chỉnh tốc độ của trò chơi để đảm bảo rằng nó chạy với FPS mong muốn.
+        pg.display.set_caption(f'{self.clock.get_fps() :.1f}')
+
+    def draw(self):
+        self.screen.fill('black')
+
+    def check_events(self):
+        for event in pg.event.get():
+            if event.type == pg.QUIT or (event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE):
+                pg.quit()
+                sys.exit()
+
+    def run(self):
+        while True:
+            self.check_events()
+            self.update()
+            self.draw()
+
+if __name__ == '__main__':
+    game = Game()
+    game.run()
