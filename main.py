b0add863a0815fa0e3da13c3dc1375e56922d721
bunrieucuaa <ann71576@gmail.com>
2024-05-21 16:00:49 +0700

    fix: create npc and make npc acttack player #15

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from sprite_object import *
 from object_handler import *
 from weapon import *
 from sound import *
+from pathfinding import *
 
 class Game:
     def __init__(self):
@@ -17,6 +18,9 @@ class Game:
         self.screen = pg.display.set_mode(RES) #Tạo một cửa sổ trò chơi với kích thước được định nghĩa ở setting
         self.clock = pg.time.Clock() #Khởi tạo một đối tượng clock để theo dõi thời gian trong trò chơi
         self.delta_time = 1
+        self.global_trigger = False #Tạo 1 biến chung để set thời gian animated die cho npc 
+        self.global_event = pg.USEREVENT + 0
+        pg.time.set_timer(self.global_event, 40)
         self.new_game()
 
     def new_game(self):
@@ -27,6 +31,8 @@ class Game:
         self.object_handler = ObjectHandler(self)
         self.weapon = Weapon(self)
         self.sound = Sound(self)
+        self.pathfinding = PathFinding(self)
+
 
 
     def update(self):
@@ -46,10 +52,13 @@ class Game:
         # self.player.draw()
 
     def check_events(self):
+        self.global_trigger = False
         for event in pg.event.get():
             if event.type == pg.QUIT or (event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE):
                 pg.quit()
                 sys.exit()
+            elif event.type == self.global_event:
+                self.global_trigger = True
             self.player.single_fire_event(event)
 
     def run(self):
