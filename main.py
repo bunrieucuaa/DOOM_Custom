f9a8e6e44d3676eb0da96aa6ed7c7e4e6e242aba
bunrieucuaa <ann71576@gmail.com>
2024-05-30 12:48:47 +0700

    feat: last commit

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ class Game:
         self.weapon = Weapon(self)
         self.sound = Sound(self)
         self.pathfinding = PathFinding(self)
-        pg.mixer.music.play(-1) #Play theme song
+        # pg.mixer.music.play(-1) #Play theme song
 
     def update(self):
         self.player.update()
